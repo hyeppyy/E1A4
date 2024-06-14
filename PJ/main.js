1de52cb17efe5c48c2814497a3f8b37b7d07c884
mutajune <june9418@naver.com>
2024-06-14 17:58:43 +0900

    버튼 추가

--- PJ/main.js
+++ PJ/main.js
@@ -6,32 +6,41 @@ console.log(numbers);
 
 let selectNum = [];
 
-for (let i = 1; i < 8; i++) {
-  let number = Math.floor(Math.random() * numbers.length);
-  let randomNum = numbers.splice(number, 1);
-  selectNum.push(randomNum);
-}
+window.addEventListener("load", function () {
+  const startBtn = document.querySelector(".random_Bt");
+  startBtn.addEventListener("click", function () {
+    raffle();
+  });
+});
 
-console.log(selectNum);
+function raffle() {
+  for (let i = 1; i < 8; i++) {
+    let number = Math.floor(Math.random() * numbers.length);
+    let randomNum = numbers.splice(number, 1);
+    selectNum.push(randomNum);
+  }
 
-const bonusNum = selectNum.splice(6, 1);
-const winNums = selectNum.sort((a, b) => a - b);
+  console.log(selectNum);
 
-for (let i = 0; i < winNums.length; i++) {
+  const bonusNum = selectNum.splice(6, 1);
+  const winNums = selectNum.sort((a, b) => a - b);
+
+  for (let i = 0; i < winNums.length; i++) {
+    setTimeout(() => {
+      const ball = document.createElement("div");
+      ball.className = "ball";
+      ball.textContent = winNums[i];
+      result.append(ball);
+    }, 1000 * (i + 1));
+  }
+
   setTimeout(() => {
-    const ball = document.createElement("div");
-    ball.className = "ball";
-    ball.textContent = winNums[i];
-    result.append(ball);
-  }, 1000 * (i + 1));
+    const bonusBall = document.createElement("div");
+    bonusBall.className = "ball";
+    bonusBall.textContent = bonusNum;
+    bonus.append(bonusBall);
+  }, 7000);
 }
-
-setTimeout(() => {
-  const bonusBall = document.createElement("div");
-  bonusBall.className = "ball";
-  bonusBall.textContent = bonusNum;
-  bonus.append(bonusBall);
-}, 7000);
 
 // to-do-list
 const form = document.querySelector("form");

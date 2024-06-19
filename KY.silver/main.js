09226c13189ea2752dbe59603513038eb3f2165d
95126m <95126m@naver.com>
2024-06-19 16:12:43 +0900

    하는중

--- KY.silver/main.js
+++ KY.silver/main.js
@@ -1,30 +1,23 @@
+let inputBox = document.getElementById('inputField');  
+let addToDo = document.getElementById('addToDo');      
+let toDoList = document.getElementById('toDoList');
 
-const container = document.querySelector('container')
-const inputfiled = document.querySelector('inputfield')
-const ul = document.querySelector('ul')
+addToDo.addEventListener('click', function () {
+    let list = document.createElement('li'); // HTML에 list 태그 추가
 
-container.addEventListener('submit', (event) => {
-    event.preventDefault();
-    if(input.value !== '') {
-    const li = document.createElement('li');
-    li.innerText = input.value;
-    ul.appendChild(li);
-    input.value = '';
+    if (!inputBox.value)
+        alert('내용을 입력해 주세요!'); // 경고창
+
+    else {
+        list.innerText = inputBox.value;        
+        toDoList.appendChild(list);   // todolist에 자식요소(list) 붙이기   
+        inputBox.value = "";
+
+        list.addEventListener('click', function () {
+            list.style.textDecoration = "line-through";
+        })
+        list.addEventListener('dblclick', function () {
+            toDoList.removeChild(list);
+        })
     }
 })
-
-
-// const form = document.querySelector('form')
-// const input = document.querySelector('input')
-// const ul = document.querySelector('ul')
-
-// form.addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     if (input.value !== '') {
-//         const li = document.createElement('li');
-//         li.innerText = input.value;
-//         ul.appendChild(li);
-//         input.value = '';
-//     }
-// });
-

e571b93d0fa9f49e0f2014c227cc6f9ec31746e0
95126m <95126m@naver.com>
2024-06-19 18:45:34 +0900

    to do 막힘

--- KY.silver/main.js
+++ KY.silver/main.js
@@ -4,6 +4,9 @@ let toDoList = document.getElementById('toDoList');
 
 addToDo.addEventListener('click', function () {
     let list = document.createElement('li'); // HTML에 list 태그 추가
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "x";
+
 
     if (!inputBox.value)
         alert('내용을 입력해 주세요!'); // 경고창
@@ -12,12 +15,38 @@ addToDo.addEventListener('click', function () {
         list.innerText = inputBox.value;        
         toDoList.appendChild(list);   // todolist에 자식요소(list) 붙이기   
         inputBox.value = "";
+        list.appendChild(deleteButton);
 
         list.addEventListener('click', function () {
             list.style.textDecoration = "line-through";
         })
-        list.addEventListener('dblclick', function () {
-            toDoList.removeChild(list);
+        deleteButton.addEventListener('click', (event) => {
+            toDoList.removeChild(event.currentTarget.parentNode); // currentTarget : 이벤트가 부착된 부모의 요소에 반환시킴
         })
-    }
+        
+    } 
 })
+inputBox.addEventListener('keypress', (event) => {
+    if (window.event.keyCode == 13) {
+    let list = document.createElement('li'); // HTML에 list 태그 추가
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "x";
+
+
+    if (!inputBox.value)
+        alert('내용을 입력해 주세요!'); // 경고창
+    
+    else {
+        list.innerText = inputBox.value;
+        toDoList.appendChild(list);   // todolist에 자식요소(list) 붙이기   
+        inputBox.value = "";
+        list.appendChild(deleteButton);
+
+        list.addEventListener('click', function () {
+            list.style.textDecoration = "line-through";
+        })
+        deleteButton.addEventListener('click', (event) => {
+            toDoList.removeChild(event.currentTarget.parentNode); // currentTarget : 이벤트가 부착된 부모의 요소에 반환시킴
+        })
+    }} 
+}) 

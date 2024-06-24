770552d3cd79b548fd610c8f6997eba0c952c078
95126m <95126m@naver.com>
2024-06-24 10:47:09 +0900

    add - time del, css and js edit

--- KY.silver/weather.js
+++ KY.silver/weather.js
@@ -1,10 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
     // 날씨 조회 함수 정의
     function getWeather(city) {
-        // API 요청해서 가져옴 (사이트에서 내 고유 key 필요)
+        // API 요청
         $.getJSON(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4eeed0ee3dd4335f4fcf46deab517c99&units=metric`)
             .done(function (result) { 
-                displayWeather(result); // 날씨 정보 표시 함수 호출
+                displayWeather(result); // 날씨 정보 표시
             })
             .fail(function(jqXHR, textStatus, errorThrown) {
                 console.error('API 요청 실패: ', textStatus, errorThrown);
@@ -14,15 +15,15 @@ $(document).ready(function() {
 
     // 날씨 정보 표시 함수 정의
     function displayWeather(result) {
+        // 기온의 소수점을 버림 Math.round 함수 사용
+        const temp = Math.round(result.main.temp);
+        const tempMin = Math.round(result.main.temp_min);
+        const tempMax = Math.round(result.main.temp_max);
+        
         $('#cityName').text(result.name);
-        $('#currentTemp').text('현재 기온 ' + result.main.temp + 'º');
-        $('#minTemp').text('최저 기온 ' + result.main.temp_min + 'º');
-        $('#maxTemp').text('최고 기온 ' + result.main.temp_max + 'º');
-
-        let measurementTime = new Date(result.dt * 1000);
-        let hours = measurementTime.getHours();
-        let minutes = measurementTime.getMinutes();
-        $('#currentTime').text('현재 시간 ' + hours + ':' + minutes);
+        $('#currentTemp').text('현재 ' + temp + 'º');
+        $('#minTemp').text('최저 ' + tempMin + 'º');
+        $('#maxTemp').text('최고 ' + tempMax + 'º');
 
         let iconUrl = `http://openweathermap.org/img/wn/${result.weather[0].icon}.png`;
         $('#weatherIcon').html(`<img src="${iconUrl}">`);
@@ -39,13 +40,13 @@ $(document).ready(function() {
             alert('지역 이름을 써조');
             return;
         }
-
         getWeather(city); // 날씨 조회 함수 호출
     });
 
-    $('#cityInput').keypress(function(event) { // enter 키 사용
+    // enter 키 사용 이벤트 추가
+    $('#cityInput').keypress(function(event) { 
         if (event.key === 'Enter') {
-            let city = $('#cityInput').val().trim(); // 함수내용이 button 클릭 이벤트와 일치해서 같은내용써줘야함!
+            let city = $('#cityInput').val().trim();
 
             if (city === '') { 
                 alert('지역 이름을 써조');
@@ -55,6 +56,6 @@ $(document).ready(function() {
             getWeather(city); 
         }
     });
-
-    $('#weatherInfo').hide(); // 입력창에 입력을 했을 경우에만 css 효과가 나타나게! (평소 숨김 상태)
+    // 입력창에 입력을 했을 경우에만 css 효과가 나타나게! (평소 숨김 상태)
+    $('#weatherInfo').hide(); 
 });
